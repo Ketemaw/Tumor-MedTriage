562d553a9e474ae9d295650407307099598e33aa
Ketemaw <ketemaw.ayimer@gmail.com>
2026-07-28 16:05:21 +0300

    frontend domain added to backend

--- backend/main.py
+++ backend/main.py
@@ -7,7 +7,7 @@ app = FastAPI(title="MedTriage API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000","https://tumormedtriage.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],

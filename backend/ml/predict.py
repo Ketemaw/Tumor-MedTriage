6f5cef681d3a5a2502aa7c88a50455718c894bf0
Ketemaw <ketemaw.ayimer@gmail.com>
2026-07-28 15:19:58 +0300

    tenserflow version changed

--- backend/ml/predict.py
+++ backend/ml/predict.py
@@ -1,10 +1,10 @@
 import os
-os.environ["TF_USE_LEGACY_KERAS"] = "1"
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 
 import tensorflow as tf
 import numpy as np
 from PIL import Image
+from tensorflow.keras.models import load_model
 from download_model import ensure_model_downloaded
 from config import settings
 
@@ -29,7 +29,7 @@ class WeightedAverageLayer(tf.keras.layers.Layer):
 
 ensure_model_downloaded()
 #loading the model
-_model = tf.keras.models.load_model(
+_model = load_model(
     settings.model_path,
     custom_objects={"WeightedAverageLayer": WeightedAverageLayer},
 )

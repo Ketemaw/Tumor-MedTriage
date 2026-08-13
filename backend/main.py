c7c05beba4e4d3b2b56a035cdff09cabeeb11253
Ketemaw <ketemaw.ayimer@gmail.com>
2026-08-13 05:04:14 +0300

    cross orgin updated

--- backend/main.py
+++ backend/main.py
@@ -7,7 +7,7 @@ app = FastAPI(title="MedTriage API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000","https://tumormedtriage-ketemaw1.vercel.app/"],
+    allow_origins=["http://localhost:3000","https://tumormedtriage-ketemaw1.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
